5957c9b63042a43678d9d5b57db31eb443eb0756
Andre Long <andre1011long@gmail.com>
2026-05-12 16:28:38 -0700

    Update managementScript.js to use Render backend URL

--- src/main/resources/static/JAVASCRIPT/managementScript.js
+++ src/main/resources/static/JAVASCRIPT/managementScript.js
@@ -6,71 +6,67 @@
    for all employees
 */
 
-window.onload = start();
+window.onload = start;
 
-function start()
-{
-	ether();
+function start() {
+    ether();
 }
 
-function ether()
-{
+function ether() {
 
     const welcomeMrManager = document.getElementById('welcomeManager');
     const managerInfoDiv = document.getElementById('managerInfo');
     const allEmployeesDiv = document.getElementById('allEmployees');
 
-    fetch('http://localhost:8080/admin/loggInUser')
-          .then(response => response.text())
-          .then(text => {
-              welcomeMrManager.innerHTML = "<h2 id='welcomeManagerDiv'>Welcome  " + text  +
-              "<form id='adminlogout' method='post' action='/admin/managerLogout'>" +
-              "<input type='submit' value='Sign Out'></form></h2>";
-          });
+    const BASE_URL = "https://render-project-szn7.onrender.com";
 
-    fetch('http://localhost:8080/admin/currentUserInfo')
-              .then(response => response.text())
-              .then(text => {
-                  managerInfoDiv.innerHTML = "<h4 id='managerInfoMessage'>  " + text + "</h4>";
-              });
+    fetch(`${BASE_URL}/admin/loggInUser`)
+        .then(response => response.text())
+        .then(text => {
+            welcomeMrManager.innerHTML = "<h2 id='welcomeManagerDiv'>Welcome  " + text  +
+                "<form id='adminlogout' method='post' action='/admin/managerLogout'>" +
+                "<input type='submit' value='Sign Out'></form></h2>";
+        });
 
-    fetch('http://localhost:8080/admin/displayAllTickets')
-                  .then(response => response.json())
-                  .then(tickets => {
-                      let info = "";
-                      tickets.forEach(ticket => {
-                           info += `
-                                      <tr class='suckerFish'>
-                                        <td class='sonOfSuckerFish'>${ticket.ticketID}</td>
-                                        <td class='sonOfSuckerFish'>${ticket.description}</td>
-                                        <td class='sonOfSuckerFish'>${ticket.ticketStatus}</td>
-                                        <td class='sonOfSuckerFish'>${ticket.amount}</td>
-                                        <td class='sonOfSuckerFish'>${ticket.dateSubmitted}</td>
-                                        <td class='sonOfSuckerFish'>${ticket.dateResolved}</td>
-                                        <td class='sonOfSuckerFish'>${ticket.reimbursementType}</td>
-                                        <td class='sonOfSuckerFish'>${ticket.employeeID}</td>
-                                      </tr>
-                                    `;
+    fetch(`${BASE_URL}/admin/currentUserInfo`)
+        .then(response => response.text())
+        .then(text => {
+            managerInfoDiv.innerHTML = "<h4 id='managerInfoMessage'>  " + text + "</h4>";
+        });
 
+    fetch(`${BASE_URL}/admin/displayAllTickets`)
+        .then(response => response.json())
+        .then(tickets => {
+            let info = "";
+            tickets.forEach(ticket => {
+                info += `
+                    <tr class='suckerFish'>
+                        <td class='sonOfSuckerFish'>${ticket.ticketID}</td>
+                        <td class='sonOfSuckerFish'>${ticket.description}</td>
+                        <td class='sonOfSuckerFish'>${ticket.ticketStatus}</td>
+                        <td class='sonOfSuckerFish'>${ticket.amount}</td>
+                        <td class='sonOfSuckerFish'>${ticket.dateSubmitted}</td>
+                        <td class='sonOfSuckerFish'>${ticket.dateResolved}</td>
+                        <td class='sonOfSuckerFish'>${ticket.reimbursementType}</td>
+                        <td class='sonOfSuckerFish'>${ticket.employeeID}</td>
+                    </tr>
+                `;
+            });
 
-                                  });
-                                  let Outer = `
-                                                <table id='AndreTable'>
-                                                    <tr>
-                                                        <th id='ticketid'>Ticket Number</th>
-                                                        <th id='description'>Ticket Description</th>
-                                                        <th id='ticketstatus'>Ticket Status</th>
-                                                        <th id='amount'>Reimbursement Amount</th>
-                                                        <th id='datesubmitted'>Date Submitted</th>
-                                                        <th id='dateresolved'>Date Resolved</th>
-                                                        <th id='reimbursementtype'>Reimbursement Type</th>
-                                                        <th id='employeeid'>Employee ID</th>
-                                                    </tr>
-                                            `;;
+            let Outer = `
+                <table id='AndreTable'>
+                    <tr>
+                        <th id='ticketid'>Ticket Number</th>
+                        <th id='description'>Ticket Description</th>
+                        <th id='ticketstatus'>Ticket Status</th>
+                        <th id='amount'>Reimbursement Amount</th>
+                        <th id='datesubmitted'>Date Submitted</th>
+                        <th id='dateresolved'>Date Resolved</th>
+                        <th id='reimbursementtype'>Reimbursement Type</th>
+                        <th id='employeeid'>Employee ID</th>
+                    </tr>
+            `;
 
-                                   allEmployeesDiv.innerHTML = Outer + info + "</table>";
-
-
-                });
-                reimbursementDiv.innerHTML = Outer + info + "</table>";
+            allEmployeesDiv.innerHTML = Outer + info + "</table>";
+        });
 }
