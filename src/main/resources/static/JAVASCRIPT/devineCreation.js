0be3204eb5cec53e9d6459ce188cc8a758fff4ac
Andre Long <andre1011long@gmail.com>
2026-05-12 17:38:02 -0700

    Update devineCreation.js to use Render backend URL

--- src/main/resources/static/JAVASCRIPT/devineCreation.js
+++ src/main/resources/static/JAVASCRIPT/devineCreation.js
@@ -3,62 +3,63 @@
    Author:   Andre Long
    Date:     10/17/2024
    Description: The purpose of this Javascript file is to display all employees
-   and to eighter create or delete employees
+   and to either create or delete employees
 */
 
-window.onload = begin();
+window.onload = begin;
 
 function begin()
 {
-   const peppermintDiv = document.getElementById('companyEmployees');
+    const peppermintDiv = document.getElementById('companyEmployees');
 
-   fetch('http://localhost:8080/admin/createDeleteEmployees')
-    .then(response => response.text())
-    .then(text => {
+    const BASE_URL = "https://render-project-szn7.onrender.com";
 
-                    let info = " ";
-                    let snoopdog = JSON.parse(text);
-                    let lengthSnoopDog = snoopdog.length;
+    fetch(`${BASE_URL}/admin/createDeleteEmployees`)
+        .then(response => response.text())
+        .then(text => {
 
-                    for(let j = 0; j < snoopdog.length; j++)
-                    {
-                        data = snoopdog[j]; //Controls which employee to display
-                        oneEmployeeArray = data.split(",");
-                        for(let i = 0; i < 1; i++)
-                        {
+            let info = " ";
+            let snoopdog = JSON.parse(text);
 
-                            info += "<tr class = 'suckerFish'>" +
-                                    "<td class='sonOfSuckerFish'>" + oneEmployeeArray[i]   + "</td>" +
-                                    "<td class='sonOfSuckerFish'>" + oneEmployeeArray[i+1] + "</td>" +
-                                    "<td class='sonOfSuckerFish'>" + oneEmployeeArray[i+2] + "</td>" +
-                                    "<td class='sonOfSuckerFish'>" + oneEmployeeArray[i+3] + "</td>" +
-                                    "<td class='sonOfSuckerFish'>" + oneEmployeeArray[i+4] + "</td>" +
-                                    "<td class='sonOfSuckerFish'>" + oneEmployeeArray[i+5] + "</td>" +
-                                    "<td class='sonOfSuckerFish'>" + oneEmployeeArray[i+6] + "</td>" +
-                                    "<td class='sonOfSuckerFish'>" + oneEmployeeArray[i+7] + "</td>" +
-                                    "<td class='sonOfSuckerFish'>" + oneEmployeeArray[i+8] + "</td>" +
-                                    "<td class='sonOfSuckerFish'>" + oneEmployeeArray[i+10] + "</td>" +
-                                    "<td class='sonOfSuckerFish'>" + oneEmployeeArray[i+11] + "</td></tr>";
-                        }
-                    }
-                    let Outer = "<table id ='AndreTable'><tr>" +
-                                "<th id='employeeid'>Employee ID</th>" +
-                                "<th id='role'>Role</th>" +
-                                "<th id='enabled'>Active</th>" +
-                                "<th id='username'>Username</th>" +
-                                "<th id='jobtitle'>Job Title</th>" +
-                                "<th id='firstname'>Firstname</th>" +
-                                "<th id='lastname'>Lastname</th>" +
-                                "<th id='phone'>Phone #</th>" +
-                                "<th id='email'>Email</th>" +
-                                "<th id='datehired'>Date Hired</th>" +
-                                "<th id='datefired'>Date Fired</th></tr>";
+            for (let j = 0; j < snoopdog.length; j++) {
 
-                    peppermintDiv.innerHTML = Outer + info + "</table>";
+                let data = snoopdog[j];
+                let oneEmployeeArray = data.split(",");
 
+                info += `
+                <tr class='suckerFish'>
+                    <td class='sonOfSuckerFish'>${oneEmployeeArray[0]}</td>
+                    <td class='sonOfSuckerFish'>${oneEmployeeArray[1]}</td>
+                    <td class='sonOfSuckerFish'>${oneEmployeeArray[2]}</td>
+                    <td class='sonOfSuckerFish'>${oneEmployeeArray[3]}</td>
+                    <td class='sonOfSuckerFish'>${oneEmployeeArray[4]}</td>
+                    <td class='sonOfSuckerFish'>${oneEmployeeArray[5]}</td>
+                    <td class='sonOfSuckerFish'>${oneEmployeeArray[6]}</td>
+                    <td class='sonOfSuckerFish'>${oneEmployeeArray[7]}</td>
+                    <td class='sonOfSuckerFish'>${oneEmployeeArray[8]}</td>
+                    <td class='sonOfSuckerFish'>${oneEmployeeArray[10]}</td>
+                    <td class='sonOfSuckerFish'>${oneEmployeeArray[11]}</td>
+                </tr>
+            `;
+            }
 
+            let Outer = `
+            <table id='AndreTable'>
+                <tr>
+                    <th id='employeeid'>Employee ID</th>
+                    <th id='role'>Role</th>
+                    <th id='enabled'>Active</th>
+                    <th id='username'>Username</th>
+                    <th id='jobtitle'>Job Title</th>
+                    <th id='firstname'>Firstname</th>
+                    <th id='lastname'>Lastname</th>
+                    <th id='phone'>Phone #</th>
+                    <th id='email'>Email</th>
+                    <th id='datehired'>Date Hired</th>
+                    <th id='datefired'>Date Fired</th>
+                </tr>
+        `;
 
-
-              });
-
+            peppermintDiv.innerHTML = Outer + info + "</table>";
+        });
 }
