38e764c3d7407c02397fb7caa35e6d110afa27c3
Andre Long <andre1011long@gmail.com>
2026-05-12 18:06:43 -0700

    Update fManagerStyles.css to use Render backend URL

--- src/main/resources/static/JAVASCRIPT/sideShow.js
+++ src/main/resources/static/JAVASCRIPT/sideShow.js
@@ -2,52 +2,55 @@
    Filename: sideShow.js
    Author:   Andre Long
    Date:     10/13/2024
-   Description: The purpose of this Javascript file is to display all images in a banner
+   Description: Smooth slideshow for banner images
 */
 
+window.onload = function() {
+      setInterval(start, 3000);
+};
 
-window.onload = setInterval(start, 3000);
-var i = 0;
-function start()
-{
+let i = 0;
+
+function start() {
 
       const imagePaths = [
-      "../IMAGES/dilbert1.jpg",
-      "../IMAGES/dilbert2.jpg",
-      "../IMAGES/dilbert3.jpg",
-      "../IMAGES/dilbert4.jpg",
-      "../IMAGES/dilbert5.jpg",
-      "../IMAGES/dilbert6.jpg",
-      "../IMAGES/dilbert7.jpg",
-      "../IMAGES/dilbert8.jpg",
-      "../IMAGES/dilbert9.jpg",
-      "../IMAGES/dilbert10.jpg",
-      "../IMAGES/dilbert11.jpg",
-      "../IMAGES/dilbert12.jpg"
+            "../IMAGES/dilbert1.jpg",
+            "../IMAGES/dilbert2.jpg",
+            "../IMAGES/dilbert3.jpg",
+            "../IMAGES/dilbert4.jpg",
+            "../IMAGES/dilbert5.jpg",
+            "../IMAGES/dilbert6.jpg",
+            "../IMAGES/dilbert7.jpg",
+            "../IMAGES/dilbert8.jpg",
+            "../IMAGES/dilbert9.jpg",
+            "../IMAGES/dilbert10.jpg",
+            "../IMAGES/dilbert11.jpg",
+            "../IMAGES/dilbert12.jpg"
       ];
 
       const description = [
-      "Motivation",
-      "Career Success",
-      "Catbert Advise",
-      "Make boss look good",
-      "Not my job",
-      "The one thing",
-      "The block chain",
-      "Management feedback",
-      "Risk Management",
-      "Lay Offs",
-      "The Dynamic",
-      "Change Management"
+            "Motivation",
+            "Career Success",
+            "Catbert Advise",
+            "Make boss look good",
+            "Not my job",
+            "The one thing",
+            "The block chain",
+            "Management feedback",
+            "Risk Management",
+            "Lay Offs",
+            "The Dynamic",
+            "Change Management"
       ];
 
-      container = document.getElementById('banner');
+      const img = document.getElementById("bannerImage");
 
-      container.innerHTML = "<img src= '" + imagePaths[i] + "'"   +  " alt = '" + description[i] + "'" + " title = '" + description[i] + "' >";
+      img.src = imagePaths[i];
+      img.alt = description[i];
+      img.title = description[i];
+
       i++;
-      if(i >= imagePaths.length - 1)
-      {
-         i = 0;
+      if (i >= imagePaths.length) {
+            i = 0;
       }
-
 }
